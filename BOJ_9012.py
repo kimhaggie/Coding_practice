bc70eacd1f003bc4cf62bbe6ab1122cbb1cf7367
kimhaggie <kimhaggie@gmail.com>
2020-11-06 00:15:48 +0900

    9012 solved

---
+++ BOJ_9012.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    deq = deque([])
+    line = sys.stdin.readline().rstrip('\n')
+    Y = True
+    for val in line:
+        if val == '(':
+            deq.append(val)
+        else:
+            if len(deq) == 0:
+                Y = False
+                break
+            else:
+                deq.pop()
+    if len(deq) != 0:
+        Y = False
+    if Y:
+        print('YES')
+    else:
+        print('NO')
+
