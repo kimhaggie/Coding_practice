696dfd5bf7591dcaa981dba71a27d14e9d8a72a7
kimhaggie <kimhaggie@gmail.com>
2020-11-09 15:23:05 +0900

    deque problem-pypy3 + revise

--- BOJ_1158.py
+++ BOJ_1158.py
@@ -2,20 +2,16 @@
 import sys
 from collections import deque
 
-n,k = list(map(int,sys.stdin.readline().rstrip('\n').split(' ')))
-deq = deque(list(range(1,n+1)))
+n,k = map(int,sys.stdin.readline().rstrip('\n').split(' '))
+deq = deque(range(1,n+1))
 cur = 1
-ans = deque([])
+answer='<'
 while len(deq)!=0:
     x = deq.popleft()
     if cur % k == 0:
-        ans.append(x)
+        answer += (str(x)+', ')
     else:
         deq.append(x)
     cur = (cur+1) % k
-print('<',end='')
-for idx,val in enumerate(ans):
-    if idx != len(ans)-1:
-        print(val,end=', ')
-    else:
-        print(val,end='>')
+answer = answer[:-2]+'>'
+print(answer)
