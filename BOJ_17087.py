455a18d807bfe57b67ab2849f5d00483c43b91c7
kimhaggie <kimhaggie@gmail.com>
2020-11-18 23:18:08 +0900

    math algorithm

---
+++ BOJ_17087.py
@@ -0,0 +1,18 @@
+#17087
+import sys
+
+def gcd(a,b):
+    while(b!=0):
+        tmp = a%b
+        a=b
+        b=tmp
+    return a
+
+N,S = map(int,sys.stdin.readline().rstrip('\n').split(' '))
+A = list(map(int,sys.stdin.readline().rstrip('\n').split(' ')))
+for idx, val in enumerate(A):
+    A[idx] = abs(val-S)
+ans = A[0]
+for val in A[1:]:
+    ans = gcd(ans,val)
+print(ans)
