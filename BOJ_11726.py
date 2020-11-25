3ac3ff252ebfbe47eb6bfb7bf5b92b00d1d8e4ac
kimhaggie <kimhaggie@gmail.com>
2020-11-25 22:14:26 +0900

    dp algorithm

---
+++ BOJ_11726.py
@@ -0,0 +1,10 @@
+#11726
+import sys
+import math
+
+N = int(sys.stdin.readline().rstrip('\n'))
+dp=[0,1,2] + [0]*(N-2)
+for i in range(1,N+1):
+    if dp[i]==0:
+        dp[i]=dp[i-2]+dp[i-1]
+print(dp[N]%10007)
