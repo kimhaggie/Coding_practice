26a042001ad35e5b20e19872c515b0eff74282af
kimhaggie <kimhaggie@gmail.com>
2020-11-25 21:12:37 +0900

    *simple algorithm + revise

--- BOJ_11653.py
+++ BOJ_11653.py
@@ -1,32 +1,9 @@
 #11653
-import sys
-import math
-
-N = int(sys.stdin.readline().rstrip('\n'))
-#cutline = max(int(math.sqrt(N+1)),N+1)
-cutline = 5000001
-prime = [1,1] + [0 for _ in range(2,cutline)]
-prime_number = []
-for i in range(2,cutline):
-    if prime[i] == 0:
-        prime[i] = 2
-        x = i + i
-        prime_number.append(i)
-        while x < cutline:
-            prime[x] = 1
-            x += i
-ans = []
-max_n = len(prime_number)
-cur = 0
-original = N
-while N != 1 and prime_number[cur] <= math.sqrt(N+1)+1:
-    if N % prime_number[cur]==0:
-        ans.append(prime_number[cur])
-        N = N/prime_number[cur]
+num = int(input())
+i = 2
+while num!=1:
+    if num%i == 0:
+        num = num/i
+        print(i)
     else:
-        cur += 1
-
-if len(ans) == 0 and N!=1:
-    print(N)
-else:
-    print('\n'.join(map(str,ans)))
+        i += 1
