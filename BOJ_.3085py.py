7aa41ca8c8b186b1e3986ebdb1edab958dff2007
kimhaggie <kimhaggie@gmail.com>
2021-01-07 13:29:41 +0900

    brute force + revise

--- BOJ_.3085py.py
+++ BOJ_.3085py.py
@@ -6,8 +6,10 @@ def find(map):
     #가로
     w = 0
     for i in range(n):
-        for j in range(n):
+        j=0
+        while(j!=n):
             if j == n-1:
+                j+=1
                 continue
             tmp = 1
             k = j + 1
@@ -20,10 +22,13 @@ def find(map):
             #print(i,j,tmp)
             if w < tmp:
                 w = tmp
+            j = k
     h = 0
     for j in range(n):
-        for i in range(n):
+        i=0
+        while(i!=n):
             if i == n-1:
+                i+=1
                 continue
             tmp = 1
             k = i + 1
@@ -36,74 +41,41 @@ def find(map):
             #print(i,j,tmp)
             if h < tmp:
                 h = tmp
+            i=k
     return max(w,h)
 
 def up(map,i,j):
     if i==0 or map[i][j]==map[i-1][j]: #못 바꿈
         return 0
-    tmp = map[i][j]
-    map[i][j] = map[i-1][j]
-    map[i-1][j] = tmp
+    map[i][j],map[i-1][j]=map[i-1][j],map[i][j]
     ans = find(map)
     #복구
-    tmp = map[i][j]
-    map[i][j] = map[i-1][j]
-    map[i-1][j] = tmp
-    return ans
-
-def down(map,i,j):
-    if i==n-1 or map[i][j]==map[i+1][j]: #못 바꿈
-        return 0
-    tmp = map[i][j]
-    map[i][j] = map[i+1][j]
-    map[i+1][j] = tmp
-    ans = find(map)
-    #복구
-    tmp = map[i][j]
-    map[i][j] = map[i+1][j]
-    map[i+1][j] = tmp
+    map[i][j],map[i-1][j]=map[i-1][j],map[i][j]
     return ans
 
 def left(map,i,j):
     if j==0 or map[i][j]==map[i][j-1]: #못 바꿈
         return 0
-    tmp = map[i][j]
-    map[i][j] = map[i][j-1]
-    map[i][j-1] = tmp
+    map[i][j],map[i][j-1]=map[i][j-1],map[i][j]
     ans = find(map)
     #복구
-    tmp = map[i][j]
-    map[i][j] = map[i][j-1]
-    map[i][j-1] = tmp
+    map[i][j],map[i][j-1]=map[i][j-1],map[i][j]
     return ans
 
-def right(map,i,j):
-    if j==n-1 or map[i][j]==map[i][j+1]: #못 바꿈
-        return 0
-    tmp = map[i][j]
-    map[i][j] = map[i][j+1]
-    map[i][j+1] = tmp
-    ans = find(map)
-    #복구
-    tmp = map[i][j]
-    map[i][j] = map[i][j+1]
-    map[i][j+1] = tmp
-    return ans
 
 
 n = int(sys.stdin.readline().rstrip('\n'))
 map = []
 for _ in range(n):
     map.append(list(sys.stdin.readline().rstrip('\n')))
-ans = []
+ans = 0
 for i in range(n):
     for j in range(n):
         u=up(map,i,j)
-        d=down(map,i,j)
         l=left(map,i,j)
-        r=right(map,i,j)
-        tmp = max([u,d,l,r])
+        tmp = max(u,l)
         if tmp <= 1:
             tmp = 0
-        ans.append(tmp)
-print(max(ans))
+        if ans<tmp:
+            ans=tmp
+print(ans)
