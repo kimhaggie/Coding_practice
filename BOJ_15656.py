b384ae6299aa1f6152d074c8895c782c7dd09313
kimhaggie <kimhaggie@gmail.com>
2021-01-09 17:54:18 +0900

    recursive algorithm

---
+++ BOJ_15656.py
@@ -0,0 +1,22 @@
+#15656
+import sys
+import math
+
+def  func(a,n,m):
+    ans = []
+    if m==1:
+        return [[a[k]] for k in range(n)]
+    else:
+        for k in range(n):
+            for x in func(a,n,m-1):
+                tmp = [a[k]]
+                tmp.extend(x)
+                ans.append(tmp)
+    return ans
+
+n,m = map(int,sys.stdin.readline().rstrip('\n').split(' '))
+a = list(map(int,sys.stdin.readline().rstrip('\n').split(' ')))
+a.sort()
+ans = func(a,n,m)
+for x in ans:
+    print(' '.join(map(str,x)))
