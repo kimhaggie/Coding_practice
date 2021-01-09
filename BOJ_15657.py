fe1614075b8e5be0d1fbb965562acfdbba26cb03
kimhaggie <kimhaggie@gmail.com>
2021-01-09 21:05:22 +0900

    *recursive algorithm

--- BOJ_15657.py
+++ BOJ_15657.py
@@ -2,21 +2,17 @@
 import sys
 import math
 
-def  func(a,i,n,m):
-    ans = []
-    if m==1:
-        return [[a[k]] for k in range(i,n)]
-    else:
-        for k in range(i,n):
-            for x in func(a,k,n,m-1):
-                tmp = [a[k]]
-                tmp.extend(x)
-                ans.append(tmp)
-    return ans
+def func(i,c):
+    if c==m:
+        print(*ans)
+        return
+    for j in range(i,n):
+        ans.append(a[j])
+        func(j,c+1)
+        ans.pop()
 
 n,m = map(int,sys.stdin.readline().rstrip('\n').split(' '))
 a = list(map(int,sys.stdin.readline().rstrip('\n').split(' ')))
 a.sort()
-ans = func(a,0,n,m)
-for x in ans:
-    sys.stdout.write(' '.join(map(str,x))+'\n')
+ans=[]
+func(0,0)
