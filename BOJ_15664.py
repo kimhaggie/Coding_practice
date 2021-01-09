06e23e91edef24f8ebdc02d90c9cd3dc60c06fd2
kimhaggie <kimhaggie@gmail.com>
2021-01-09 20:21:36 +0900

    recursive algorithm

---
+++ BOJ_15664.py
@@ -0,0 +1,21 @@
+#15664
+import sys
+import math
+
+def func(a,i,c):
+    if c==m:
+        if ''.join(map(str,ans)) not in total:
+            print(*ans)
+            total.add(''.join(map(str,ans)))
+        return
+    for j in range(i,n):
+        ans.append(a[j])
+        func(a,j+1,c+1)
+        ans.pop()
+
+n,m = map(int,sys.stdin.readline().rstrip('\n').split(' '))
+a = list(map(int,sys.stdin.readline().rstrip('\n').split(' ')))
+a.sort()
+total = set()
+ans=[]
+func(a,0,0)
