ec0c86246145bc956381a396e2a06258276c6ccb
kimhaggie <kimhaggie@gmail.com>
2021-01-13 17:06:40 +0900

    *brute force + revise

--- BOJ_2529.py
+++ BOJ_2529.py
@@ -5,34 +5,24 @@ def func(a,start,m,ans,op):
     max_val = -1
     min_val = float('inf')
     if m == 0:
-        poss = True
-        for idx,val in enumerate(op):
-            if val == '<':
-                if ans[idx] >= ans[idx+1]:
-                    poss = False
-                    break
-            else:
-                if ans[idx] <= ans[idx+1]:
-                    poss = False
-                    break
-        if poss:
-            return ''.join(map(str,ans))
-        else:
-            return '-1'
+        return ''.join(map(str,ans))
     else:
+        cur_op = op[len(op)-m]
         for idx in range(start,len(a)):
+            if len(ans)!=0:
+                if cur_op == '<' and ans[-1] >= a[idx]:
+                    continue
+                if cur_op == '>' and ans[-1] <= a[idx]:
+                    continue
             ans.append(a[idx])
             b = a.copy()
             del b[idx]
             sub_ans = func(b,0,m-1,ans,op)
             if type(sub_ans) is str: #마지막 깊이에서 리턴되었을 때
-                if sub_ans == '-1':
-                    pass
-                else:
-                    if min_val == float('inf') or int(sub_ans) < int(min_val):
-                        min_val = sub_ans
-                    if int(max_val) < int(sub_ans):
-                        max_val = sub_ans
+                if min_val == float('inf') or int(sub_ans) < int(min_val):
+                    min_val = sub_ans
+                if int(max_val) < int(sub_ans):
+                    max_val = sub_ans
             else: #[min_val,max_val]을 리턴했을 때
                 if sub_ans[0] == float('inf'):
                     pass
