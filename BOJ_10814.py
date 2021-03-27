d2ef6000d8c84ba69d47150cf9e184962b1cb837
kimhaggie <kimhaggie@gmail.com>
2021-03-27 21:49:26 +0900

    *sort algorithm

---
+++ BOJ_10814.py
@@ -0,0 +1,16 @@
+#10814
+import sys
+from collections import deque
+import math
+import copy
+import heapq
+
+N = int(sys.stdin.readline().rstrip('\n'))
+A = []
+for _ in range(N):
+    x = list(sys.stdin.readline().rstrip('\n').split(' '))
+    x[0]=int(x[0])
+    A.append(x)
+A=sorted(A,key=lambda x:x[0])
+for val in A:
+    print(*val)
